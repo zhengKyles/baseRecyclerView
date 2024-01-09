c99c1d25b2b7b53a5833a512a8a43b2a3abefc44
1239878682@qq.com <Zidanshen55>
2024-01-09 14:24:15 +0800

    加入maven

--- baserecyclerview/build.gradle.kts
+++ baserecyclerview/build.gradle.kts
@@ -35,8 +35,9 @@ android {
 
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     api("io.github.cymchad:BaseRecyclerViewAdapterHelper:4.0.1")
     api("androidx.recyclerview:recyclerview:1.3.1")
+    implementation("com.github.jitpack:gradle-simple:1.0")
 }
