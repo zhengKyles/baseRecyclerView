58fa58dad42c9c9e47532fa496d2eaa05972849c
1239878682@qq.com <Zidanshen55>
2024-01-09 16:11:14 +0800

    更新为viewBinding

---
+++ baserecyclerview/src/main/java/com/kyle/baserecyclerview/BaseAdapter.kt
@@ -0,0 +1,34 @@
+package com.kyle.baserecyclerview
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import com.chad.library.adapter4.BaseQuickAdapter
+
+/**
+ * Created by Kyle on 2018/9/13.
+ */
+abstract class BaseAdapter<T : Any,D:ViewBinding> :
+    BaseQuickAdapter<T, BaseAdapter.MyViewHolder<D>>() {
+
+    class MyViewHolder<D:ViewBinding>(
+        val binding: D,
+    ) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(
+        context: Context,
+        parent: ViewGroup,
+        viewType: Int
+    ): MyViewHolder<D> {
+        return MyViewHolder(initViewBinding(parent, LayoutInflater.from(parent.context)))
+    }
+
+    abstract fun initViewBinding(parent: ViewGroup, inflater: LayoutInflater): D
+    override fun onBindViewHolder(holder: MyViewHolder<D>, position: Int, item: T?) {
+        convert(holder.binding, position, item)
+    }
+
+    protected abstract fun convert(binding: D, position: Int, item: T?)
+}
