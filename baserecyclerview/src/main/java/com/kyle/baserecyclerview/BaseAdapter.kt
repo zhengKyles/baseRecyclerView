9a3b9f6242d8886c11e8fa0ca08b2ee3b39da999
1239878682@qq.com <Zidanshen55>
2024-04-25 10:47:18 +0800

    更新版本到2.0.2

--- baserecyclerview/src/main/java/com/kyle/baserecyclerview/BaseAdapter.kt
+++ baserecyclerview/src/main/java/com/kyle/baserecyclerview/BaseAdapter.kt
@@ -3,19 +3,23 @@ package com.kyle.baserecyclerview
 import android.view.View
 import androidx.viewbinding.ViewBinding
 import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
 
 /**
  * Created by Kyle on 2018/9/13.
  */
 abstract class BaseAdapter<T, D : ViewBinding>(layoutResId: Int) :
-    BaseQuickAdapter<T, MyViewHolder<D>>(layoutResId, ArrayList()) {
-    override fun createBaseViewHolder(view: View): MyViewHolder<D> {
-        return MyViewHolder(view)
+    BaseQuickAdapter<T, BaseViewHolder>(layoutResId, ArrayList()) {
+    override fun createBaseViewHolder(view: View): BaseViewHolder {
+        return BaseViewHolder(view)
     }
 
-    override fun convert(helper: MyViewHolder<D>, item: T) {
-        convert(helper.binding as D, helper.adapterPosition, item)
+    override fun convert(holder: BaseViewHolder, item: T) {
+        val binding = initBinding(holder.itemView)
+        convert(binding, holder.absoluteAdapterPosition, item)
     }
+
+    abstract fun initBinding(itemView: View): D
 
     protected abstract fun convert(binding: D, position: Int, item: T)
 }
